2dc36f5017bc76e8f4c39db39119b5d0be7af987
Michelle Arkhangorodsky <m.arkhangorodsky@gmail.com>
2014-10-26 18:38:45 -0400

    Fixed problem with initial commiting

--- server.R
+++ server.R
@@ -1,17 +1,27 @@
 library(shiny)
-library(datasets)
 
 # Define server logic required to summarize and view the selected dataset
 shinyServer(function(input, output) {
   
   # Return the requested dataset
   datasetInput <- reactive({
-    switch(input$dataset,
-           "rock" = rock,
-           "pressure" = pressure,
-           "cars" = cars)
+    switch(input$sequence,
+           "p53" = p53,
+           "Glycophorin A" = glyA,
+           "something else" = se)
   })
-  
+  # Return the hydrophobicity value of a primary sequence 11 amino acids long
+  y <- function(x, primary_sequence)
+    scores <- c(4.5, 4.2, 3.8, 2.8, 2.5, 1.9, 1.8, -0.4, -0.7, -0.9, -0.8, -1.3, -1.6, -3.2, -3.5, -3.5, -3.5, -3.5, -3.9, -4.5)
+    names(scores) <- c('I', 'V', 'L', 'F','C','M','A', 'G', 'T',
+                        'W', 'S', 'Y', 'P', 'H', 'E', 'Q', 'D', 'N', 'K', 'R')
+    list_to_sum <- []
+    for (n in (x-6):(x+5){
+      list_to_sum.append(scores[substr(primary_sequence, n, n)])
+    return round(sum(list_to_sum)/11,1)
+    }
+
+    
   # Generate a summary of the dataset
   output$summary <- renderPrint({
     dataset <- datasetInput()
