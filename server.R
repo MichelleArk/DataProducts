78bc2a1783acb5942cdde6634820a2c42d54b015
Michelle Arkhangorodsky <m.arkhangorodsky@gmail.com>
2014-11-21 12:02:04 -0500

    "Added functionality for any inputted protein sequence"

--- server.R
+++ server.R
@@ -1,15 +1,9 @@
 library(shiny)
-
 # Define server logic required to summarize and view the selected dataset
 shinyServer(function(input, output) {
   
-  # Return the requested dataset
-  datasetInput <- reactive({
-    switch(input$sequence,
-           "p53" = p53,
-           "Glycophorin A" = glyA,
-           "something else" = se)
-  })
+  sequence <- reactive({ sequence <- c(input$sequence)})
+  sequence2 <- reactive({ sequence2 <- c(input$sequence2)})
   # Return the hydrophobicity value of a primary sequence 11 amino acids long
   iscore <- function(x, primary_sequence) {
     scores <- c(4.5, 4.2, 3.8, 2.8, 2.5, 1.9, 1.8, -0.4, -0.7, -0.9, -0.8, -1.3, -1.6, -3.2, -3.5, -3.5, -3.5, -3.5, -3.9, -4.5)
@@ -40,18 +34,27 @@ shinyServer(function(input, output) {
   
   #Plot the values as a line graph, with straight line at y=0
   summary <- function(primary_sequence){
-    plot(getx(primary_sequence), gety(primary_sequence), type = "n", xlab="Sequence Number", ylab = "Hydropathic Index")
+    plot(getx(primary_sequence), gety(primary_sequence), type = "n", xlab="Sequence Number", ylab = "Hydropathic Index", ylim = c(-4,4))
     lines(getx(primary_sequence), gety(primary_sequence), type = "l")
     abline(0,0)
   }
+sequence <- reactive({
+  if(input$sequence2 != ""){
+    sequence <- gsub(" ", "",toupper(input$sequence2))
+  }  else sequence <- input$sequence
+  sequence
+})
   
-
+  output$summary <- renderPlot({
+    summary(sequence())
+    })
   
   # Generate a summary of the dataset
-  output$summary <- renderPlot({
-    plot(getx(input$sequence), gety(input$sequence), type = "n", xlab="Sequence Number", ylab = "Hydropathic Index", ylim = c(-4,4))
-    lines(getx(input$sequence), gety(input$sequence), type = "l")
-    abline(0,0)
-  })
-  
+ # output$summary2 <- renderPlot({
+  #  summary(gsub(" ", "",toupper(input$sequence2)))
+  #}
+
+  #output$summary <- renderPlot({
+  #  summary(input$sequene)
+  #})
 })
